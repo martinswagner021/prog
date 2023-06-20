ca37180a7347a17cb9b5055e51704eb0c90d87d7
Wagner Martins <martinswagner021@gmail.com>
2023-06-20 01:21:38 -0300

    Adding doggo properties :)

--- Java/java swing/Main.java
+++ Java/java swing/Main.java
@@ -1,5 +1,7 @@
+import java.net.MalformedURLException;
+
 public class Main{
-    public static void main(String argv[]){
+    public static void main(String argv[]) throws MalformedURLException{
         Frame myFrame = new Frame();
         Label1 hello = new Label1();
         myFrame.add(hello);
