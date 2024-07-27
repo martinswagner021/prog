436977774675b234ea91f8fb1cf792e7efb40108
Wagner Martins <martinswagner021@gmail.com>
2024-07-27 00:58:32 -0300

    Atividade BD

--- Databases/aula_bd_queries.sql
+++ Databases/aula_bd_queries.sql
@@ -6,6 +6,7 @@ select f.cidade, p.cidade from fornecedor as f, peca as p;
 
 select nome from fornecedor, fornece where codigo = codF and codP="P1";
 
+-- Tarefa 1
 -- 1)
 select distinct fornecedor.codigo from fornecedor, fornece, peca where
 	fornecedor.codigo = codF and
@@ -24,4 +25,44 @@ select fornecedor.nome from fornecedor, fornece, peca where
     peca.codigo = codP and
     peca.codigo = "P1";
 
+-- Tarefa 2
+-- 1)
+select p.nome, p.peso, p.cidade from peca as p where p.codigo in (
+	select codP from fornece where codF in (
+		select f.codigo from fornecedor as f where f.cidade = "Rio das Ostras"
+	)
+);
 
+-- 2) IN
+select f.nome from fornecedor as f where f.codigo not in (
+	select codF from fornece
+);
+
+-- 2) EXISTS
+select f.codigo from fornecedor as f where not exists (
+	select * from fornece where f.codigo = codF
+);
+
+-- 3)
+select max(peso) from peca where  (
+	
+);
+
+-- 4) IN
+select f.nome from fornecedor as f where f.codigo in (
+	select codF from fornece
+) and f.codigo not in (
+	select codF from fornece where codP = "p1"
+);
+
+-- 4) EXISTS
+select f.nome from fornecedor as f where exists (
+	select * from fornece where f.codigo = codF
+) and not exists (
+	select * from fornece where f.codigo = codF and codP = "p1"
+);
+
+-- 5)
+select p.nome from peca as p where p.peso = (
+	select min(p.peso) from peca as p
+);
