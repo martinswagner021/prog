38d4126ffe3d077febe99826be5eeb919d2c5b55
Wagner Martins <martinswagner021@gmail.com>
2024-08-01 18:28:52 -0300

    ler_transacoesCSV

--- File Programming/main.cpp
+++ File Programming/main.cpp
@@ -11,9 +11,7 @@
 #include <map>
 #include <ctime>
 
-using std::cout;
-using std::vector;
-using std::string;
+using namespace std;
 
 struct Transacao {
     int dia, mes, ano;
@@ -31,29 +29,27 @@ struct movimentacao_consolidada {
 
 vector<Transacao> ler_transacoesCSV(const string &nome_arquivo) {
     vector<Transacao> transacoes;
-    std::ifstream arquivo(nome_arquivo);
+    ifstream arquivo(nome_arquivo);
     string linha;
 
     if (!arquivo.is_open()) {
-        std::cerr << "Erro ao abrir o arquivo CSV" << std::endl;
+        cout << "Erro ao abrir o arquivo CSV." << endl;
         return transacoes;
     }
 
-    while (std::getline(arquivo, linha)) {
-        std::istringstream ss(linha);
+    while (getline(arquivo, linha)) {
+        stringstream ss(linha);
         string campo;
         Transacao transacao;
         
-        getline(ss, campo, ','); transacao.dia = std::stoi(campo);
-        getline(ss, campo, ','); transacao.mes= std::stoi(campo);
-        getline(ss, campo, ','); transacao.ano = std::stoi(campo);
-        getline(ss, campo, ','); transacao.agencia_origem = std::stoi(campo);
-        getline(ss, campo, ','); transacao.conta_origem = std::stoi(campo);
-        getline(ss, campo, ','); transacao.valor = std::stoi(campo);
-        getline(ss, campo, ',');   
-        transacao.agencia_destino = campo.empty() ? 0 : std::stoi(campo);
-        getline(ss, campo, ',');
-        transacao.conta_destino = campo.empty() ? 0 : std::stoi(campo);
+        getline(ss, campo, ','); transacao.dia = stoi(campo);
+        getline(ss, campo, ','); transacao.mes= stoi(campo);
+        getline(ss, campo, ','); transacao.ano = stoi(campo);
+        getline(ss, campo, ','); transacao.agencia_origem = stoi(campo);
+        getline(ss, campo, ','); transacao.conta_origem = stoi(campo);
+        getline(ss, campo, ','); transacao.valor = stod(campo);
+        getline(ss, campo, ','); transacao.agencia_destino = campo.empty() ? 0 : stoi(campo);
+        getline(ss, campo, ','); transacao.conta_destino = campo.length() <= 1 ? 0 : stoi(campo);
         
         transacoes.push_back(transacao);
     }
@@ -70,13 +66,13 @@ então se inicia uma nova movimentação consolidada depois add a transação pa
 eletronicas OU se a agencia conta/destino for = 0 então é só add o valor para subtotal dinheiro vivo
 add total de transações e retorna o map consolidados */
 
-std::map<std::pair<int, int>, movimentacao_consolidada> consolidarTransacoes(const vector<Transacao>& transacoes, int mes, int ano){
-    std::map<std::pair<int, int>, movimentacao_consolidada> consolidados;
+map<pair<int, int>, movimentacao_consolidada> consolidarTransacoes(const vector<Transacao>& transacoes, int mes, int ano){
+    map<pair<int, int>, movimentacao_consolidada> consolidados;
 
     for ( const auto& transacao : transacoes) {
         if (transacao.mes == mes && transacao.ano == ano) {
-        std::pair<int, int> chave_origem(transacao.agencia_origem, transacao.conta_origem);
-        std::pair<int, int> chave_destino(transacao.agencia_destino, transacao.conta_destino);
+        pair<int, int> chave_origem(transacao.agencia_origem, transacao.conta_origem);
+        pair<int, int> chave_destino(transacao.agencia_destino, transacao.conta_destino);
         
     if (consolidados.find(chave_origem) == consolidados.end()) {
             consolidados[chave_origem] = movimentacao_consolidada{
@@ -105,10 +101,10 @@ std::map<std::pair<int, int>, movimentacao_consolidada> consolidarTransacoes(con
     return consolidados;
 }
 
-void salvarConsolidadoBinario(const string& nome_arquivo, const std::map<std::pair<int, int>, movimentacao_consolidada>& consolidados){
-    std::ofstream arquivo(nome_arquivo, std::ios::binary);
+void salvarConsolidadoBinario(const string& nome_arquivo, const map<pair<int, int>, movimentacao_consolidada>& consolidados){
+    ofstream arquivo(nome_arquivo, ios::binary);
     if(!arquivo.is_open()){
-        std::cerr << "Erro ao abrir o arquivo binário para escrita." << std::endl;
+        cerr << "Erro ao abrir o arquivo binário para escrita." << endl;
         return;
     }
 
@@ -118,17 +114,17 @@ void salvarConsolidadoBinario(const string& nome_arquivo, const std::map<std::pa
     arquivo.close();
 }
 
-std::map<std::pair<int, int>, movimentacao_consolidada> carregarConsolidadoBinario(const string& nome_arquivo){
-    std::map<std::pair<int, int>, movimentacao_consolidada> consolidados;
-    std::ifstream arquivo(nome_arquivo, std::ios::binary);
+map<pair<int, int>, movimentacao_consolidada> carregarConsolidadoBinario(const string& nome_arquivo){
+    map<pair<int, int>, movimentacao_consolidada> consolidados;
+    ifstream arquivo(nome_arquivo, ios::binary);
 
     if(!arquivo.is_open()){
-        std::cerr << "Erro ao abrir o arquivo binário para carregar." << std::endl;
+        cerr << "Erro ao abrir o arquivo binário para carregar." << endl;
         return consolidados;
     }
     movimentacao_consolidada consolidado;
     while(arquivo.read(reinterpret_cast<char*>(&consolidado), sizeof(movimentacao_consolidada))){
-        std::pair<int, int> chave  = {consolidado.agencia, consolidado.conta};
+        pair<int, int> chave  = {consolidado.agencia, consolidado.conta};
         consolidados[chave] = consolidado;
     }
 
@@ -139,5 +135,9 @@ std::map<std::pair<int, int>, movimentacao_consolidada> carregarConsolidadoBinar
 
 
 int main(){
-    ler_transacoesCSV("transacoes.csv");
+    vector<Transacao> t = ler_transacoesCSV("transacoes.csv");
+
+    int N = 3;
+    Transacao p = t[N];
+    cout << p.dia << " " << p.mes << " " << p.ano << " " << p.agencia_origem << " " << p.conta_origem << " " << p.valor << " " << p.agencia_destino << " " << p.conta_destino << " " << endl;
 }
